66a2a4d75b402c3df09ae407d7be2a4d43b67041
Gustas Janušauskas <gustasjanusauskas@gmail.com>
2022-10-24 21:52:50 +0300

    added a project showcase section, 2 projects

---
+++ portfoliosite_frontend/src/app/classes/project.ts
@@ -0,0 +1,12 @@
+import { Image } from './image';
+
+export interface Project {
+    id: number;
+
+    title: string;
+    description: string;
+    tools: string;
+    link: string;
+
+    images:Image[];
+}
