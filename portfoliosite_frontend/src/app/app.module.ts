04df170f03cc27c77f3768460695c4e8f9791827
Gustas Janušauskas <gustasjanusauskas@gmail.com>
2022-12-05 15:42:16 +0200

    Removed form, added email link instead

--- portfoliosite_frontend/src/app/app.module.ts
+++ portfoliosite_frontend/src/app/app.module.ts
@@ -1,30 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
-import { ContactComponent } from './contact/contact.component';
-
-import { ContactsService } from "./services/contacts.service";
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    PagenotfoundComponent,
-    ContactComponent
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [ContactsService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
