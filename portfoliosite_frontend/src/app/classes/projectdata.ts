925e40e4674b07e4c92fe8e18d8ee37ace0436d2
Gustas Janušauskas <gustasjanusauskas@gmail.com>
2023-03-07 21:10:59 +0200

    converted images to jpg

--- portfoliosite_frontend/src/app/classes/projectdata.ts
+++ portfoliosite_frontend/src/app/classes/projectdata.ts
@@ -6,9 +6,9 @@ export const PROJECT_DATA: Project[] = [
         title:'Social Media Site Mockup',
         description:'Created a fully functional social media site from the ground up, including a back-end powered by NodeJS and PostgreSQL, and a front-end powered by Angular, using Material components.\n\nThe website features a working account, profile, post, messaging and search system, packaged within a modern and responsive UX.',
         images:[
-            {path:'assets/project_socialmediasite_1.png'},
-            {path:'assets/project_socialmediasite_2.png'},
-            {path:'assets/project_socialmediasite_3.png'}
+            {path:'assets/project_socialmediasite_1.jpg'},
+            {path:'assets/project_socialmediasite_2.jpg'},
+            {path:'assets/project_socialmediasite_3.jpg'}
         ],
         tools:'Angular • Material • NodeJS • PostgreSQL',
         link:'https://github.com/GustasJanusauskas/Social-Media-Site'
@@ -18,9 +18,9 @@ export const PROJECT_DATA: Project[] = [
         title:'Library Catalogue Mockup',
         description:'A fictional library book catalogue, with account, borrowing and reserving functionality, as well as an in-depth search system and a dedicated admin panel.',
         images:[
-            {path:'assets/project_bookcatalogue_1.png'},
-            {path:'assets/project_bookcatalogue_2.png'},
-            {path:'assets/project_bookcatalogue_3.png'}
+            {path:'assets/project_bookcatalogue_1.jpg'},
+            {path:'assets/project_bookcatalogue_2.jpg'},
+            {path:'assets/project_bookcatalogue_3.jpg'}
         ],
         tools:'Angular • Bootstrap • Material • ASP.NET • PostgreSQL',
         link:'https://github.com/GustasJanusauskas/Book-Catalog-Service'
