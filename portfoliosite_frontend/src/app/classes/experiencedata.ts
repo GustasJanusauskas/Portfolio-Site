349fde218510a7e74b6c37fb1595660a6fe6a7ff
Gustas Janušauskas <gustasjanusauskas@gmail.com>
2023-09-18 15:12:36 +0300

    Updated info

--- portfoliosite_frontend/src/app/classes/experiencedata.ts
+++ portfoliosite_frontend/src/app/classes/experiencedata.ts
@@ -8,7 +8,7 @@ export const EXPERIENCE_DATA: Experience[] = [
         location:'Vilnius, Lithuania',
         responsibilities: [
             'Developed and maintained several .NET applications using C# and WPF',
-            'Maintained and extended MS SQL Server databases with a combined 500+ stored procedures',
+            'Maintained and extended MS SQL Server databases with a combined >5000 stored procedures',
             'Closely worked with clients to negotiate and implement new features'
         ],
         date: new ExpDate(new Date(2023,6),new Date())
