6586ddfb70ba5d571321df46a81181e205787084
Gustas Janušauskas <gustasjanusauskas@gmail.com>
2024-02-27 17:58:37 +0200

    update

--- portfoliosite_frontend/src/app/classes/experiencedata.ts
+++ portfoliosite_frontend/src/app/classes/experiencedata.ts
@@ -3,14 +3,14 @@ import { Experience, ExpDate } from "./experience";
 export const EXPERIENCE_DATA: Experience[] = [
     {
         id:0,
-        title:'.NET Developer',
+        title:'Full-Stack Software Engineer',
         company:'UAB „Higso Bozonas“',
         location:'Vilnius, Lithuania',
         responsibilities: [
             'Designed new features for and maintained several .NET WPF applications',
             'Wrote an inventorization system, integrated with SQL database',
             'Worked with accountants to improve debt tracking system',
-            'Created prepaid invoice system, improved PDF forming feature',
+            'Taught interns and new employees',
             'Maintained and optimised MS SQL Server procedures'
         ],
         date: new ExpDate(new Date(2023,6),new Date())
